72b1804dc3344b012cbf9d15b295bfd3f921386e
aestq <aestq.dev@gmail.com>
2023-10-19 21:54:09 +0300

    add alias paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
@@ -9,5 +10,11 @@ export default defineConfig({
   },
   build: {
     outDir: 'build'
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+    alias: {
+      '@': path.resolve(__dirname, 'src')
+    }
   }
 })
